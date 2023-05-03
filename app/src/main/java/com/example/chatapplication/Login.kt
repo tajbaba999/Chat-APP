cbcdd902549a101bff437fdb50a120a0e6197c74
tajbaba999 <tajbaba153@gmail.com>
2023-05-04 00:22:56 +0530

    Second commit

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import java.net.PasswordAuthentication
 
 class Login : AppCompatActivity() {
@@ -14,9 +16,15 @@ class Login : AppCompatActivity() {
     private lateinit var btnsignup : Button
     private lateinit var btnlogin : Button
 
+    private lateinit var mAuth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         editEmail = findViewById(R.id.edit_email)
         editPasswordAuthentication = findViewById(R.id.editTextTextPassword)
@@ -27,5 +35,25 @@ class Login : AppCompatActivity() {
             val intent = Intent(this,Signup::class.java)
             startActivity(intent)
         }
+
+        btnlogin.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPasswordAuthentication.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    private fun login(Email : String, password : String){
+        mAuth.signInWithEmailAndPassword(Email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login,"Some error occrured with Sigin", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/chatapplication/Signup.kt
+++ app/src/main/java/com/example/chatapplication/Signup.kt
@@ -1,11 +1,75 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Signup : AppCompatActivity() {
+
+
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editPasswordAuthentication: EditText
+    private lateinit var btnsignup: Button
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        editEmail = findViewById(R.id.edit_email)
+        editPasswordAuthentication = findViewById(R.id.editTextTextPassword)
+        btnsignup = findViewById(R.id.btnsignup)
+        btnsignup = findViewById(R.id.btnsignup)
+        editName = findViewById(R.id.edit_Name)
+
+        btnsignup.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPasswordAuthentication.text.toString()
+            val name = editName.text.toString()
+
+            signUp(name, email, password)
+        }
     }
+
+    private fun signUp(name: String, email: String, password: String) {
+        //login for loggind user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //jumping to home activity
+                    addusertoDatabse(name, email, mAuth.currentUser?.uid!!)
+                    val intent = Intent(this@Signup, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(
+                        this@Signup,
+                        "Some error occrured with Signup",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    private fun addusertoDatabse(name: String, email: String, uid: String?) {
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        if (uid != null) {
+            mDbRef.child("user").child(uid).setValue(User(name,email,uid))
+        }
+    }
+
+
 }
